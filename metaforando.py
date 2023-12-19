242a366930c9e9a73f68460696a128ccce1d38f2
kimimorgam <140536250+kimimorgam@users.noreply.github.com>
2023-12-19 09:37:19 -0300

    Add files via upload

--- metaforando.py
+++ metaforando.py
@@ -1,3 +1,4 @@
+
 import os
 import shutil
 import tkinter as tk
@@ -10,7 +11,6 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
-
 # Função para baixar o modelo pré-treinado
 def download_model():
     url = "https://github.com/priya-dwivedi/face_and_emotion_detection/raw/master/emotion_detector_models/model_v6_23.hdf5"
@@ -20,13 +20,11 @@ def download_model():
         f.write(r.content)
     print("Modelo baixado.")
 
-
 # Função para carregar o modelo
 def load_pretrained_model():
     model = load_model("model_v6_23.hdf5")
     print("Modelo carregado.")
     return model
-
 
 # Função para verificar disponibilidade da GPU
 def check_gpu():
@@ -36,7 +34,6 @@ def check_gpu():
     else:
         print("GPU está disponível. O processo será executado na GPU.")
 
-
 # Função para abrir o vídeo para análise
 def open_video():
     global cap
@@ -45,7 +42,6 @@ def open_video():
     print(f"Vídeo {file_path} aberto.")
     return cap
 
-
 # Função para obter taxa de quadros do vídeo
 def get_video_fps():
     global video_fps
@@ -53,22 +49,17 @@ def get_video_fps():
     fps_label.config(text=f"Taxa de quadros do vídeo: {video_fps}")
     return video_fps
 
-
 # Função para coletar entrada do usuário para taxa de quadros
 def get_user_fps():
     user_fps = fps_entry.get()
     return user_fps
-
 
 # Função para o loop de análise de quadro
 def analyze_frame(cap, model, analyze_fps, video_fps):
     frame_count = 0
     total_emotions = {}
     os.makedirs("frames", exist_ok=True)
-
-    frame_total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    progress_bar["maximum"] = frame_total
-
+    
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -80,9 +71,9 @@ def analyze_frame(cap, model, analyze_fps, video_fps):
             faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
 
             for (x, y, w, h) in faces:
-                roi_gray = gray_frame[y:y + h, x:x + w]
+                roi_gray = gray_frame[y:y+h, x:x+w]
                 roi_gray = cv2.resize(roi_gray, (48, 48))
-                roi = roi_gray.astype('float') / 255.0
+                roi = roi_gray.astype('float')/255.0
                 roi = img_to_array(roi)
                 roi = np.expand_dims(roi, axis=0)
 
@@ -94,40 +85,28 @@ def analyze_frame(cap, model, analyze_fps, video_fps):
                 total_emotions[emotion] = total_emotions.get(emotion, 0) + 1
                 cv2.rectangle(frame, (x, y - 40), (x + w, y), (0, 0, 255), -1)
                 cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
-
-                # Atualiza a barra de progresso
-                progress_bar["value"] = cap.get(cv2.CAP_PROP_POS_FRAMES)
-
-                # Atualiza o tempo estimado
-                time_elapsed = frame_count / video_fps
-                time_remaining = (cap.get(cv2.CAP_PROP_FRAME_COUNT) - frame_count) / video_fps
-                time_label.config(
-                    text=f"Tempo decorrido: {time_elapsed:.2f} segundos\nTempo estimado restante: {time_remaining:.2f} segundos")
-
+            
             cv2.imwrite(f"frames/frame_{frame_count}.jpg", frame)
 
         frame_count += 1
-        progress_bar.update()
 
     return total_emotions
-
 
 # Função para salvar o vídeo analisado
 def save_analyzed_video(cap):
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     out = cv2.VideoWriter('final_video.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame_width, frame_height))
-
+    
     frame_files = [f for f in os.listdir("frames") if f.endswith(".jpg")]
     frame_files.sort(key=lambda x: int(x.split("_")[1].split(".")[0]))
 
     for frame_file in frame_files:
         frame = cv2.imread(f"frames/{frame_file}")
         out.write(frame)
-
+    
     out.release()
     print("Vídeo analisado salvo.")
-
 
 # Função para mostrar estatísticas
 def show_statistics(total_emotions):
@@ -138,15 +117,12 @@ def show_statistics(total_emotions):
     else:
         print("Nenhuma estatística de análise disponível. Verifique se a análise facial foi bem-sucedida.")
 
-
 # Função principal para executar todas as tarefas
 def main():
     global cap, video_fps
     open_video()
     get_video_fps()
-
-
-# Função para iniciar a análise
+    
 def start_analysis():
     download_model()
     model = load_pretrained_model()
@@ -157,7 +133,6 @@ def start_analysis():
     show_statistics(total_emotions)
     shutil.rmtree("frames")  # Remove o diretório com os quadros extraídos
     print("Processo concluído.")
-
 
 # Interface Tkinter
 root = tk.Tk()
@@ -170,12 +145,6 @@ fps_label = tk.Label(root, text="Taxa de quadros do vídeo: ")
 fps_label.pack()
 fps_entry = tk.Entry(root)
 fps_entry.pack()
-
-progress_bar = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate")
-progress_bar.pack()
-
-time_label = tk.Label(root, text="Tempo decorrido: 0.00 segundos\nTempo estimado restante: 0.00 segundos")
-time_label.pack()
 
 analyze_button = tk.Button(root, text="Iniciar Análise", command=start_analysis)
 analyze_button.pack()
